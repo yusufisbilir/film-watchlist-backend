8b047dbdb4c35fecded3416ea54dee3d1e11c33a
yusuf isbilir <dev.yusufisbilir@gmail.com>
2025-12-06 17:06:14 +0300

    add some router

--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-const express = require('express')
-const chalk = require('chalk')
+import express from 'express'
+import chalk from 'chalk'
+import moviesRouter from './routes/movies.route.js'
 
 const app = express()
 
+// API Routes
+app.use('/movies', moviesRouter)
+
+// Health Check Endpoint
 app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+// Start the server
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
