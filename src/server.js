5650737423f4216e6211c763b56bae97b163b04f
yusuf isbilir <dev.yusufisbilir@gmail.com>
2025-12-06 16:31:34 +0300

    add health check

--- src/server.js
+++ src/server.js
@@ -3,6 +3,10 @@ const chalk = require('chalk')
 
 const app = express()
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 const PORT = process.env.PORT || 5001
 
 app.listen(PORT, () => {
