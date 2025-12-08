929204f0152ba760f1dffb28ffd275824ddcd9ec
yusuf isbilir <dev.yusufisbilir@gmail.com>
2025-12-08 22:10:02 +0300

    migrate all tables

---
+++ prisma/migrations/20251208190801_add_tables/migration.sql
@@ -0,0 +1,40 @@
+-- CreateEnum
+CREATE TYPE "WatchlistItemStatus" AS ENUM ('PLANNED', 'WATCHING', 'COMPLETED', 'DROPPED');
+
+-- CreateTable
+CREATE TABLE "Movie" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "overview" TEXT,
+    "releaseYear" INTEGER NOT NULL,
+    "genres" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "runtime" INTEGER,
+    "posterUrl" TEXT,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Movie_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "WatchlistItem" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "movieId" TEXT NOT NULL,
+    "status" "WatchlistItemStatus" NOT NULL DEFAULT 'PLANNED',
+    "rating" INTEGER,
+    "notes" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "WatchlistItem_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Movie" ADD CONSTRAINT "Movie_createdBy_fkey" FOREIGN KEY ("createdBy") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WatchlistItem" ADD CONSTRAINT "WatchlistItem_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "WatchlistItem" ADD CONSTRAINT "WatchlistItem_movieId_fkey" FOREIGN KEY ("movieId") REFERENCES "Movie"("id") ON DELETE CASCADE ON UPDATE CASCADE;
